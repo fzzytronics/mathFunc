9ed44de721e5cabacc058f75547dc5f8bc38d224
fzzytronics <pabloguz72562@gmail.com>
2025-07-15 14:34:53 -0500

    Calculates circle properties
    Adds a script that calculates the area, volume, and
    surface area of a circle based on user input.
    
    The script prompts the user for the radius and then
    calculates and prints the area, volume, and surface area
    using the `math` module.

---
+++ theMathPart/circle.py
@@ -0,0 +1,21 @@
+from math import *
+
+r = float(input())
+
+class area():
+    area = pi * r**2
+    print(area)
+
+area()
+
+class volume():
+    volume = (4/3) * pi * r**3
+    print(volume)
+
+volume()
+
+class surfaceArea():
+    surfaceArea = 4 * pi * r**2
+    print(surfaceArea)
+
+surfaceArea()
---
+++ theMathPart/triangle.py
@@ -0,0 +1,24 @@
+from math import *
+
+b = float(input())
+h = float(input())
+r = float(input())
+
+j = float(r**2)
+k = float(h**2)
+
+class triArea():
+    triArea = (1/2) * b * h
+    print(triArea)
+triArea()
+
+class triVol():
+    triVol = (1/3) * pi * r**2 * h
+    print(triVol)
+triVol()
+
+class triSurfaceArea():
+    triSArea = pi * r * (r + (sqrt(j*k)))
+    print(triSArea)
+triSurfaceArea()
+
